9661147ec8a4a76888d17b1afc0f12bf80edf57f
jlopezlash <jlopezlash1@educacion.navarra.es>
2024-11-15 09:39:24 +0100

    Create Ejercicio01.py

---
+++ Ejercicio01.py
@@ -0,0 +1,7 @@
+#Crea un programa que solicte al usuario su comida favorita y almacene
+#la entrada en una variable
+comida = input('Dime cuál es tu comida favorita ')
+print('Me encanta', comida ,'también!')
+años = int(input('¿Cuantos años tienes? '))
+meses = años * 12
+print('Has vivido aproximadamente', meses ,'meses')
