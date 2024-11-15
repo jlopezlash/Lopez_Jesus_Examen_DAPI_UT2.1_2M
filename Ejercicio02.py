d09e993bff90247b921597487a3dcc2946e42493
jlopezlash <jlopezlash1@educacion.navarra.es>
2024-11-15 09:39:28 +0100

    Create Ejercicio02.py

---
+++ Ejercicio02.py
@@ -0,0 +1,9 @@
+'''NombreC = input('Dime tu nómbre en formato Nombre Apellido ')
+Nombre = NombreC.split()
+print(Nombre[0])'''
+Palabra = input('Dime una palabra ')
+Vocal = input('Dime una vocal de la palabra ')
+for letra in Palabra:
+    if letra == Vocal:
+        letra = letra.upper()
+print(Palabra)
